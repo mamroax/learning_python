ddc43ddf5ce030f120e6ad630d39d8a43fc08e20
mamroax <mamroax@gmail.com>
2023-02-19 18:28:41 +0300

    +

---
+++ name.py
@@ -0,0 +1,9 @@
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(f"Hello, {full_name.title()}!")
+full_name = "{} {}".format(first_name, last_name)
+print((((((((f"Hello, {full_name.title()}!"))))))))
+universe_age = 14_000_000_000
+print(universe_age)
+print(type(universe_age))
